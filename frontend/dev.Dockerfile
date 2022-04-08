f160971c1aceb0e04d8a6145c05fc8da30cd08d8
cihat <cihat02020202@gmail.com>
2022-04-08 23:31:04 +0300

    Initial commit

---
+++ frontend/dev.Dockerfile
@@ -0,0 +1,15 @@
+FROM node:16-alpine
+
+WORKDIR /app
+
+ADD package.json package-lock.json ./
+
+RUN apk add --update python3 make g++ && rm -rf /var/cache/apk/*
+
+RUN npm install -g npm@latest
+
+RUN npm install
+
+ADD .browserslistrc .prettierrc .eslintrc.js babel.config.js vue.config.js ./
+
+CMD [ "npm", "run", "serve" ]
---
+++ frontend/src/components/hello-world.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+}
+</script>
+
+<template lang="pug">
+.hello
+  h1 {{ msg }}
+  p Merhaba arkadaslar ben Furkan Torun. 
+  img(src='https://media-exp1.licdn.com/dms/image/C4E03AQEdWqW36P9FUw/profile-displayphoto-shrink_400_400/0/1615576718166?e=1654732800&v=beta&t=gQWlDtlJcfnjk4ArocWohfesDm4lSU_V7UQaDCkMHkQ')
+</template>
