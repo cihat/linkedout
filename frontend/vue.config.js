29b63ece3218fd7d798e24c348ebd6e322194c8e
cihat <cihat02020202@gmail.com>
2022-04-10 02:07:05 +0300

    Fix hot-reload for docker and vuejs but not work

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   devServer: {
     disableHostCheck: true,
+    // disableHostCheck: true,
+    // hot: true,
+    // watchOptions: {
+    //   aggregateTimeout: 300,
+    //   poll: 1000,
+    // },
   },
   lintOnSave: false,
 }
