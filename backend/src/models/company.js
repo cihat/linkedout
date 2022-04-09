6de9c37e7fef636ba1233ad51e368e83fe18ee62
cihat <cihat02020202@gmail.com>
2022-04-10 02:05:10 +0300

    Add company model

---
+++ backend/src/models/company.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const autopopulate = require('mongoose-autopopulate')
+const Schema = mongoose.Schema
+
+const companySchema = new Schema(
+  {
+    comment: {
+      type: 'ObjectId',
+      ref: 'Comment',
+      autopopulate: true,
+    },
+    employeeNumber: Number,
+    name: String,
+  },
+  { timestamps: true }
+)
+
+companySchema.plugin(autopopulate)
+
+module.exports = mongoose.model('Company', companySchema)
