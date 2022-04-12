aa1df598183e5d75012a862677c9d50e3c7baef9
cihat <cihat02020202@gmail.com>
2022-04-12 18:16:55 +0300

    Add vue-icomoon package

---
+++ frontend/src/components/Icon/index.vue
@@ -0,0 +1,27 @@
+<script>
+import { Icomoon } from 'vue-icomoon'
+import iconSet from './selection.json'
+
+export default {
+  name: 'Icon',
+  components: {
+    Icomoon,
+  },
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    return {
+      props,
+      iconSet,
+    }
+  },
+}
+</script>
+
+<template>
+  <icomoon :iconSet="iconSet" v-bind="props" />
+</template>
