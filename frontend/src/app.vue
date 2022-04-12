78fbe0bc8fba483198ce49e7d0c21214fc0114d8
cihat <cihat02020202@gmail.com>
2022-04-12 15:48:13 +0300

    Divide the whole ready-made design into components

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,12 +1,26 @@
+<script>
+import Header from './components/Header/index.vue'
+export default {
+  name: 'app',
+  components: {
+    Header,
+  },
+}
+</script>
+
 <template lang="pug">
 #app
-  #nav
-    router-link(to="/") Home
-    span &nbsp;|&nbsp;
-    router-link(to="/about") About
-  router-view
+  .layout
+    Header
+    router-view
 </template>
 
 <style lang="scss">
 @import '@/assets/theme.scss';
+.layout {
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: auto 1fr;
+}
 </style>
