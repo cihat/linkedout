77db5aa34f67035431195783a142b461e4a80032
cihat <cihat02020202@gmail.com>
2022-04-13 04:33:16 +0300

    Add ant design entegrations

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,50 @@
 import { createApp } from 'vue'
-import App from './app.vue'
+import App from './App.vue'
 import './register-service-worker'
 import router from './router'
 import store from './store'
 import 'normalize.css'
+import Antd from 'ant-design-vue'
 
-createApp(App)
-  .use(store)
-  .use(router)
-  .mount('#app')
+import {
+  Layout,
+  Button,
+  Result,
+  Card,
+  Divider,
+  Col,
+  Row,
+  Drawer,
+  Table,
+  Form,
+  InputNumber,
+  Input,
+  Alert,
+  Tag,
+} from 'ant-design-vue'
+
+const components = [
+  Layout,
+  Button,
+  Result,
+  Card,
+  Divider,
+  Col,
+  Row,
+  Drawer,
+  Table,
+  Form,
+  InputNumber,
+  Input,
+  Alert,
+  Tag,
+]
+
+const app = createApp(App)
+
+components.map(c => app.use(c))
+app.config.productionTip = false
+
+app.use(store)
+app.use(router)
+app.mount('#app')
