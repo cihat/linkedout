557c5d2cad205852b0f0ac0b2fe9f6b11d67f036
cihat <cihat02020202@gmail.com>
2022-04-14 13:50:05 +0300

    Add services architecture for mongoose models

---
+++ backend/src/services/base-service.js
@@ -0,0 +1,36 @@
+class BaseService {
+  constructor(model) {
+    this.model = model
+    this.filename = model.name.toLowerCase()  
+  }
+
+  async save(objects) {
+    return await this.model.insertMany(objects)
+  }
+
+  async load() {
+    return this.model.find()
+  }
+
+  async insert(object) {
+    return await this.model.create(object)
+  }
+
+  async removeBy(property, value) {
+    return this.model.deleteOne({ [property]: value })
+  }
+
+  async update(id, object) {
+    return await this.model.findByIdAndUpdate(id, object)
+  }
+
+  async find(id) {
+    return await this.model.findById(id)
+  }
+
+  async findBy(property, value) {
+    return this.model.findById({ [property]: value })
+  }
+}
+
+module.exports = BaseService
